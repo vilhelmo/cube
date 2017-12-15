80a1ed5673119c390a4bb5f5c9b707e708a507e1
Vilhelm Hedberg <vilhelm_hedberg@apple.com>
2017-12-14 19:56:53 -0800

    Initial Commit

---
+++ cube/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cube
+//
+//  Created by Vilhelm Hedberg on 12/14/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
